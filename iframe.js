b98d62172d4985c05475ede064b9a61c1de148ec
dimitry.kamarnikov <dimitry.kamarnikov@ironsrc.com>
2019-09-30 17:10:12 +0300

    onload reemit after applying external css

--- iframe.js
+++ iframe.js
@@ -4,4 +4,8 @@ function handleMessage(msg) {
   const style = document.querySelector('#external')
 
   style.innerHTML = msg.data
+  
+  const evt = document.createEvent('Event')
+  evt.initEvent('load', false, false)
+  window.dispatchEvent(evt)
 }
