8519ca259d7cbf0e26348af81819eb7b19010552
dimitry.kamarnikov <dimitry.kamarnikov@ironsrc.com>
2019-09-30 16:37:01 +0300

    load external css added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const apiUrl = 'https://eu-gb.functions.cloud.ibm.com/api/v1/web/mosquid%40gmail.com_dev/load-external-css/weather.json';
+
 (function() {
   initDOMEvents()
 })()
@@ -8,9 +10,34 @@ function dispatch(ctx, e) {
   ctx.postMessage(val, '*')
 }
 
+function setIframeHeight(iframe) {
+  iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px'
+}
+
+async function getExternalCSS() {
+  const input = document.querySelector('input')
+  const val = input.value
+  
+  if (!val) return
+  
+  const data = await fetch(apiUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      url: val
+    })
+  }).then(e => e.json())
+
+  if (!data.body.code) return
+  
+  document.querySelector('textarea').value = data.body.code
+  document.querySelector('textarea').dispatchEvent(new Event('change'))
+}
+
 function initDOMEvents() {
   const textarea = document.querySelector('#css')
   const iframe = document.querySelector('iframe')
+
   const cWin = iframe.contentWindow
 
   textarea.addEventListener('change', dispatch.bind(null, cWin))
